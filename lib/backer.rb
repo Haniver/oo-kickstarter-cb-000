6fdfe706162b3927553373efebaba1446ffc3ffe
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-25 16:15:56 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,13 @@
 class Backer
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
   def back_project(title)
     self.backed_projects << Project.retrieve_or_create_by_title(title)
   end
+
 end
